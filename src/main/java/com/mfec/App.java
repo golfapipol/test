eae33d5806507a68a4fc594da96ac67240a1e699
Apipol Sukgler <golf.apipol@gmail.com>
2014-08-04 10:36:12 +0700

    first init

---
+++ src/main/java/com/mfec/App.java
@@ -0,0 +1,18 @@
+package com.mfec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static int sum(int num1,int num2)
+    {
+    	int result = num1+num2;
+    	return result;
+    }
+}
